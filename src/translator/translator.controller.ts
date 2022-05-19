51d9629f6ca8e88a5c80a2cf7f7bac3913fafcfa
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-19 09:09:06 +0200

    add translate()

--- src/translator/translator.controller.ts
+++ src/translator/translator.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { TranslateLanguage } from './TranslateLanguage';
+import { TranslatorService } from './translator.service';
 
 @Controller('translator')
-export class TranslatorController {}
+export class TranslatorController {
+  constructor(private readonly translatorService: TranslatorService) {}
+
+  @Post()
+  @HttpCode(201)
+  translate(@Body() body: TranslateLanguage) {}
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { setMainConfig } from '../src/main.config';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,13 +13,41 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    setMainConfig(app);
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/translator [POST]', () => {
+    it('properly sends request to endpoint', async () => {
+      const { body: response, status: status } = await request(
+        app.getHttpServer(),
+      )
+        .post('/translator')
+        .send({ language: 'en' });
+
+      expect(status).toBe(201);
+    });
+
+    it('throws error when inappropriate data send', async () => {
+      const { body: response, status: status } = await request(
+        app.getHttpServer(),
+      )
+        .post('/translator')
+        .send({ language: 123 });
+
+      expect(status).toBe(400);
+      expect(response).toEqual({
+        error: 'Bad Request',
+        message: [
+          'language must be longer than or equal to 2 characters',
+          'language must be a string',
+        ],
+        statusCode: 400,
+      });
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
