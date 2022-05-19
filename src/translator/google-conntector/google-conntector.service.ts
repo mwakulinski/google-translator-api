2daa4580b7024cbe4e473fe3793b69852f6a3448
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-19 15:14:57 +0200

    implements GoogleConnectorService

--- src/translator/google-conntector/google-conntector.service.ts
+++ src/translator/google-conntector/google-conntector.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { TranslateLanguage } from '../TranslateLanguage';
+import { v2 } from '@google-cloud/translate';
 
 @Injectable()
-export class GoogleConntectorService {}
+export class GoogleConntectorService {
+  createTranslatorService() {
+    const projectId = process.env.PROJECT_ID;
+    const credentials = JSON.parse(process.env.CREDENTIALS!);
+    return new v2.Translate({
+      credentials,
+      projectId,
+    });
+  }
+}
