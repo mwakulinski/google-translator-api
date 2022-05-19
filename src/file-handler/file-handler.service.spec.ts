09c80a6f59c8fba618e700099bb665dd9d7744c5
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-19 23:05:32 +0200

    implements writeToFile()

--- src/file-handler/file-handler.service.spec.ts
+++ src/file-handler/file-handler.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FileHandlerService } from './file-handler.service';
+import * as fs from 'fs';
+
+// jest.mock('fs', () => ({
+//   promises: {
+//     writeFile: jest.fn(),
+//     readFile: jest.fn(),
+//     mkdir: jest.fn(),
+//   },
+//   existsSync: jest.fn(),
+// }));
 
 describe('FileHandlerService', () => {
   let service: FileHandlerService;
+  fs.promises.writeFile = jest.fn();
+  fs.promises.readFile = jest.fn();
+  fs.promises.mkdir = jest.fn();
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [FileHandlerService],
     }).compile();
-
     service = module.get<FileHandlerService>(FileHandlerService);
+  });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('if folder exists should not create it and should write input data to the file with given name', async () => {
+    //@ts-ignore
+    fs.existsSync = jest.fn().mockReturnValue(false);
+    await service.writeToFile('texts', 'en.json', { ok: 'daradę' });
+    expect(fs.promises.mkdir).toHaveBeenCalledTimes(1);
+    expect(fs.promises.writeFile).toHaveBeenCalledTimes(1);
+  });
+
+  it("if folder doesn't exist should create it and should write input data to the file with given name", async () => {
+    //@ts-ignore
+    fs.existsSync = jest.fn().mockReturnValue(true);
+    await service.writeToFile('texts', 'en.json', { ok: 'daradę' });
+    expect(fs.promises.mkdir).toHaveBeenCalledTimes(0);
+    expect(fs.promises.writeFile).toHaveBeenCalledTimes(1);
   });
 });
--- src/file-handler/file-handler.service.ts
+++ src/file-handler/file-handler.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { dir } from 'console';
+import * as fs from 'fs';
+const path = require('path');
 
 @Injectable()
-export class FileHandlerService {}
+export class FileHandlerService {
+  async writeToFile<T>(dirName: string, fileName: string, input: T) {
+    // try {
+    //   const response = fs.promises.stat(path.resolve(dirName));
+    //   console.log(response);
+    // } catch (error) {
+    //   await fs.promises.mkdir(path.resolve(dirName));
+    // }
+
+    try {
+      if (!fs.existsSync(path.resolve(dirName))) {
+        await fs.promises.mkdir(path.resolve(dirName));
+      }
+      await fs.promises.writeFile(
+        path.resolve(dirName, fileName),
+        JSON.stringify(input),
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+}
