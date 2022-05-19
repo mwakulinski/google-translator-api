2863f1822a978d16abcd1deb0bf95cb2c2463057
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-19 10:26:41 +0200

    add configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TranslatorModule } from './translator/translator.module';
 
 @Module({
-  imports: [TranslatorModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TranslatorModule],
   controllers: [AppController],
   providers: [AppService],
 })
