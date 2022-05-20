bf65306a3c4dd51ee7dc58c6a8033346e927c0e0
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-20 17:59:11 +0200

    import FileHAndlerModule

--- src/translator/translator.module.ts
+++ src/translator/translator.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TranslatorService } from './translator.service';
 import { TranslatorController } from './translator.controller';
 import { GoogleConntectorService } from './google-conntector/google-conntector.service';
+import { FileHandlerModule } from '../file-handler/file-handler.module';
 
 @Module({
+  imports: [FileHandlerModule],
   providers: [TranslatorService, GoogleConntectorService],
-  controllers: [TranslatorController]
+  controllers: [TranslatorController],
 })
 export class TranslatorModule {}
