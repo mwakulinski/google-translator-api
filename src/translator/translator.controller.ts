829c8a3328316cbf60d27dc19514e4bba3d4eef6
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-23 14:42:54 +0200

    update translate() in TranslatorController

--- src/translator/translator.controller.ts
+++ src/translator/translator.controller.ts
@@ -1,13 +1,23 @@
 import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { FileHandlerService } from 'src/file-handler/file-handler.service';
 import { createTranslateDto } from './dto/create-translate.dto';
 import { TranslatorService } from './translator.service';
 
 @Controller('translator')
 export class TranslatorController {
-  constructor(private readonly translatorService: TranslatorService) {}
+  constructor(
+    private readonly translatorService: TranslatorService,
+    private readonly fileHandlerService: FileHandlerService,
+  ) {}
 
   @Post()
   async translate(@Body() body: createTranslateDto) {
-    return await this.translatorService.translate(body);
+    const objectToTranslate = JSON.parse(
+      await this.fileHandlerService.readFile('texts', 'pl.json'),
+    );
+    return await this.translatorService.getTranslatedData(
+      body,
+      objectToTranslate,
+    );
   }
 }
