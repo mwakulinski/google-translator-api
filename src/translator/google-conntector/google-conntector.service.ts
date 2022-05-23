19303765ad0571f95afede0801f25a9f87ca4a9f
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-23 14:45:04 +0200

    implement translate() in GoogleConnectorService

--- src/translator/google-conntector/google-conntector.service.ts
+++ src/translator/google-conntector/google-conntector.service.ts
@@ -4,12 +4,20 @@ import { v2 } from '@google-cloud/translate';
 
 @Injectable()
 export class GoogleConntectorService {
-  createTranslatorService() {
+  private createTranslatorService() {
     const projectId = process.env.PROJECT_ID;
     const credentials = JSON.parse(process.env.CREDENTIALS!);
     return new v2.Translate({
       credentials,
       projectId,
     });
+  }
+
+  async translate(textToTranslate: string[], language: string) {
+    const [response] = await this.createTranslatorService().translate(
+      textToTranslate,
+      language,
+    );
+    return response;
   }
 }
